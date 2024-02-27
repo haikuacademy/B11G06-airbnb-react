a0b8e985ada7812891e3edcd86df8d2be69248ba
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-27 11:48:17 +0700

    create a house file and a function in it, updeted the routes file and import the house route

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home'
+import House from './components/House'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/houses/:id' element={<House />} />
             </Routes>
         </BrowserRouter>
     )
