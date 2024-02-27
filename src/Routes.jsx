655037d283df857f1503c6afcf6c265c86e91dd3
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-27 12:23:22 +0700

    create the login route and imported in the routes files

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import House from './components/House'
 import Houses from './components/Houses'
 import Listings from './components/Listings'
+import Login  from './components/Login'
 
 function Router() {
     return (
@@ -11,6 +12,7 @@ function Router() {
                 <Route path='/' element={<Houses />} />
                 <Route path='/houses/:id' element={<House />} />
                 <Route path='/listings' element={<Listings />} />
+                <Route path='/login' element={<Login />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/Login.jsx
@@ -0,0 +1,10 @@
+function Login() {
+    return (
+        <div>
+            <h1>
+                Hello from Login!
+            </h1>
+        </div>
+    )
+}
+export default Login
