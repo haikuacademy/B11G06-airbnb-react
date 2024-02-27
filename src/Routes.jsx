9bc24539ce7ea6de697d62ad11eaae51624cbe41
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-02-27 11:44:29 +0700

    create signup

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 import Home from './components/Home'
+import Signup from './components/Signup'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path='/' element={<Home />} />
+                <Route path='/signup' element={<Signup />} />
             </Routes>
         </BrowserRouter>
     )
---
+++ src/components/Signup.jsx
@@ -0,0 +1,9 @@
+function Signup() { 
+    return (
+        <div>
+            <p>Hello from Signup!</p>
+        </div>
+    )
+}
+
+export default Signup
