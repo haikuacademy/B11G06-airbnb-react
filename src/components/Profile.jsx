02822e81883f9bba5bc1ea77bf871d5ee37cf663
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-27 14:26:01 +0700

    style the profile section

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,9 +1,10 @@
-function Profile() { 
-    return (
-        <div>
-            <p>Hallo from Profile</p>
-        </div>
-    )
+function Profile() {
+  return (
+    <div className="flex flex-row bg-blue-200 gap-2">
+      <div className=" bg-blue-600">1</div>
+      <div className=" bg-blue-600">2</div>
+    </div>
+  )
 }
 
 export default Profile
