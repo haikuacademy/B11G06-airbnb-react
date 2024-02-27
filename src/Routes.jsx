09d301eb2fd4b4b5899433b870c88dff9e31c93c
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-27 11:42:54 +0700

    rename the function and file home to houses

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -1,12 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import Home from './components/Home'
+import Houses from './components/Houses'
 
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/' element={<Home />} />
+                <Route path='/' element={<Houses />} />
             </Routes>
         </BrowserRouter>
     )
