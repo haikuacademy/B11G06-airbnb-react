6d4b11a4939439ba443531061c9e980a7f085ce5
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-27 13:31:39 +0700

    adding style to the house route

--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -1,9 +1,25 @@
 function Houses() {
   return (
     <div>
-      <p>Hello from Houses!</p>
+      <div className=" flex justify-between bg-blue-200">
+        <div className="bg-blue-600">1</div>
+        <div className="bg-blue-600">2</div>
+        <div className="bg-blue-600">3</div>
+        <div className="bg-blue-600">4</div>
+        <div className="bg-blue-600">5</div>
+        <div className="bg-blue-600">6</div>
+      </div>
+      <div className=" grid grid-cols-5 grid-rows-2 bg-pink-200 gap-2">
+        <div className="bg-pink-600">1</div>
+        <div className="bg-pink-600">2</div>
+        <div className="bg-pink-600">3</div>
+        <div className="bg-pink-600">4</div>
+        <div className="bg-pink-600">5</div>
+        <div className="bg-pink-600">6</div>
+        <div className="bg-pink-600">7</div>
+      </div>
     </div>
-  )
+    )
 }
 
 export default Houses
