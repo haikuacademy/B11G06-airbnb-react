11c94271d186057f942cfb95c9614e19b5251452
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-02-28 12:16:48 +0700

    style review reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,9 +1,18 @@
 function Reviews() {
   return (
     <div>
-      <div className="grid grid-cols-3 gap-32 bg-pink-200">
+      <div className="grid grid-cols-3 gap-x-32 p-2">
+        <div className="col-span-2">
+          <h3 className="text-lg font-bold">Reviews</h3>
+          <p className="text-xs">4.5</p>
+        </div>
+        <div className="border p-4">
+          <p className="text-sm font-bold">Leave a review</p>
+          <div>
+            <p className="text-xs">0</p>
+          </div>
+        </div>
         <Review />
-        <div className="bg-pink-600">6</div>
       </div>
     </div>
   )
@@ -11,10 +20,23 @@ function Reviews() {
 
 function Review() {
   return (
-    <div className="bg-pink-600 col-span-2 ">
-      <div className="flex gap-2 justify-start bg-blue-200">
+    <div className="border col-span-2 p-2 ">
+      <div className="flex gap-2 justify-start">
         <div className="bg-blue-600">5.1</div>
-        <div className="bg-blue-600">5.2</div>
+        <div>
+          <p className="text-xs text-gray-400">22 Jan 2024</p>
+          <h6 className="text-xs">Mark Lino</h6>
+        </div>
+      </div>
+      <div className="pt-2">
+        <p className="text-xs">5</p>
+      </div>
+      <div className="pt-2">
+        <p className="text-xs leading-1 ">
+          Great place to stay! The house is very clean and comfortable, and the
+          location is perfect. the host was very friendly and helful. Highly
+          recommend!
+        </p>
       </div>
     </div>
   )
