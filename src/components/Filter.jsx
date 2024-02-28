387ac67340aaeca434bcf65043c4e700d49eff3a
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-02-28 10:47:27 +0700

    create Filter file and export the function  to houses file

---
+++ src/components/Filter.jsx
@@ -0,0 +1,13 @@
+function Filter() {
+  return (
+    <div className=" flex justify-between bg-blue-200">
+      <div className="bg-blue-600">1</div>
+      <div className="bg-blue-600">2</div>
+      <div className="bg-blue-600">3</div>
+      <div className="bg-blue-600">4</div>
+      <div className="bg-blue-600">5</div>
+      <div className="bg-blue-600">6</div>
+    </div>
+  )
+}
+export default Filter
--- src/components/Houses.jsx
+++ src/components/Houses.jsx
@@ -1,3 +1,4 @@
+import Filter from './Filter'
 import HouseCard from './HouseCard'
 import NavBar from './Nav'
 
@@ -5,14 +6,7 @@ function Houses() {
   return (
     <div>
       <NavBar />
-      <div className=" flex justify-between bg-blue-200">
-        <div className="bg-blue-600">1</div>
-        <div className="bg-blue-600">2</div>
-        <div className="bg-blue-600">3</div>
-        <div className="bg-blue-600">4</div>
-        <div className="bg-blue-600">5</div>
-        <div className="bg-blue-600">6</div>
-      </div>
+      <Filter />
       <div className=" grid grid-cols-5 grid-rows-2 bg-pink-200 gap-2">
         <HouseCard />
         <HouseCard />
