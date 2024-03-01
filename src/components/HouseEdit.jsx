677fe80257fad8525819cc797b2163c598aed222
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-03-01 10:35:34 +0700

    fix the issue and instead of two forms build all in just one to let the client edit a house

--- src/components/HouseEdit.jsx
+++ src/components/HouseEdit.jsx
@@ -4,103 +4,97 @@ function HouseEdit() {
   return (
     <div className=" mx-20 mt-2">
       <NavBar />
-      <div className="border">
-        <div className="grid grid-cols-2 gap-28">
-          <div className=" p-2">
-            <span className=" text-base ">Edit your listing</span>
-            <form className="mt-2">
-              <div className="text-sm text-gray-400">Location</div>
-              <input
-                className="border rounded p-1 w-full"
-                type="text"
-                placeholder="Bali.."
-              />
-              <div className="text-sm text-gray-400">Bedrooms</div>
-              <input
-                className="border rounded p-1 w-full"
-                type="number"
-                placeholder="2"
-              />
-              <div className="text-sm text-gray-400">Bathrooms</div>
-              <input
-                className="border rounded p-1 w-full"
-                type="number"
-                placeholder="2"
-              />
-              <div className="text-sm text-gray-400">Price per Night</div>
-              <input
-                className="border rounded p-1 w-full"
-                type="number"
-                placeholder="140"
-              />
-              <div className="text-sm text-gray-400">Description</div>
-              <textarea
-                className="border h-20 p-1 w-full"
-                name="house-description"
-                cols="30"
-                rows="10"
-                placeholder="Please write a description..."
-              ></textarea>
-              <div className=" flex gap-2">
-                <button className="border rounded p-2 text-white bg-[#FB7185]">
-                  Save Changes
-                </button>
-                <button className="border rounded p-2">Cancel</button>
-              </div>
-            </form>
-          </div>
-          <div className="p-5">
-            <div className="text-sm text-gray-400">Photos</div>
-            <form className=" flex flex-col">
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-              <input
-                type="url"
-                placeholder="http://.."
-                className="border rounded mb-2 p-1"
-              />
-            </form>
+      <form className="border grid grid-cols-2 gap-28 mt-2 p-3">
+        <div>
+          <div className="mb-2">Edit your listing</div>
+          <div className="text-sm mt-2 text-gray-400">Location</div>
+          <input
+            className="border rounded p-1 w-full"
+            type="text"
+            placeholder="Bali.."
+          />
+          <div className="text-sm mt-2 text-gray-400">Bedrooms</div>
+          <input
+            className="border rounded p-1 w-full"
+            type="number"
+            placeholder="2"
+          />
+          <div className="text-sm mt-2 text-gray-400">Bathrooms</div>
+          <input
+            className="border rounded p-1 w-full"
+            type="number"
+            placeholder="2"
+          />
+          <div className="text-sm mt-2 text-gray-400">Price per Night</div>
+          <input
+            className="border rounded p-1 w-full"
+            type="number"
+            placeholder="140"
+          />
+          <div className="text-sm mt-2 text-gray-400">Description</div>
+          <textarea
+            className="border h-28 p-1 w-full"
+            name="house-description"
+            cols="30"
+            rows="10"
+            placeholder="Please write a description..."
+          ></textarea>
+          <div className=" flex gap-2">
+            <button className="border rounded p-2 text-white bg-[#FB7185]">
+              Save Changes
+            </button>
+            <button className="border rounded p-2">Cancel</button>
           </div>
         </div>
-      </div>
+        <div className=" flex flex-col p-2 my-5">
+          <span className="text-sm text-gray-400">Photos</span>
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+          <input
+            type="url"
+            placeholder="http://.."
+            className="border rounded mb-2 p-1"
+          />
+        </div>
+      </form>
     </div>
   )
 }
