30f392c81989ed9961bc21e2de6e51a93c0d92f7
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-03-01 10:39:05 +0700

    create listings form

--- src/components/Listings.jsx
+++ src/components/Listings.jsx
@@ -4,18 +4,69 @@ function Listings() {
   return (
     <div>
       <NavBar />
-      <div className="grid grid-cols-2 bg-pink-200 gap-44">
-        <div className=" bg-pink-600">1</div>
-        <div className=" bg-pink-600">2</div>
-      </div>
-      <div className="grid grid-cols-5 bg-pink-200 gap-4">
-        <div className=" bg-pink-600">1</div>
-        <div className=" bg-pink-600">2</div>
-        <div className=" bg-pink-600">3</div>
-        <div className=" bg-pink-600">4</div>
-        <div className=" bg-pink-600">5</div>
-        <div className=" bg-pink-600">6</div>
-        <div className=" bg-pink-600">7</div>
+      <div className="container mx-auto">
+        <div className="border-2 rounded-sm p-4">
+          <h1 className="text-sm mb-4">List a House</h1>
+          <div className="grid grid-cols-2 gap-32">
+            <div>
+              <label className="text-xs text-gray-400 ">Location</label>
+              <input className="w-full border-2 rounded-sm p-2"></input>
+              <label className="text-xs text-gray-400 ">Bedrooms</label>
+              <input className="w-full border-2 rounded-sm p-2"></input>
+              <label className="text-xs text-gray-400 ">Bathrooms</label>
+              <input className="w-full border-2 rounded-sm p-2"></input>
+              <label className="text-xs text-gray-400 ">Price per Night</label>
+              <input className="w-full border-2 rounded-sm p-2"></input>
+              <label className="text-xs text-gray-400 ">Description</label>
+              <textarea
+                rows="3"
+                className="w-full border-2 rounded-sm p-2"
+              ></textarea>
+              <button className="bg-[#FB7185] text-white p-2 rounded-sm mt-2">
+                List house
+              </button>
+            </div>
+            <div>
+              <label className="text-xs text-gray-400">Photos</label>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+              <input
+                type="link"
+                className="w-full border-2 rounded-sm p-2 mb-1"
+              ></input>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
