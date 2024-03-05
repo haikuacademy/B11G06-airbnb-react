34290bb9e388eefaf38886b2e204177c56d98702
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-03-05 13:52:09 +0700

    made the gallery interactive meaning that now the client is available to click the small picture in the gallery and get a preview on the bigger windows

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,37 +1,29 @@
+import { useState } from 'react'
+
 function Gallery(props) {
+  const [selectedImage, setSelectedImage] = useState(props.image[0])
+
   return (
     <div className=" grid grid-cols-2 gap-8">
       <div className="border rounded">
-        <img src={props.image[0]} alt="house-1" />
+        <img
+          className="rounded-md w-full h-full"
+          src={selectedImage}
+          alt="house"
+        />
       </div>
       <div className=" grid grid-cols-3 gap-2">
-        <div className="border rounded">
-          <img src={props.image[0]} alt="house-1" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[1]} alt="house-2" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[2]} alt="house-3" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[3]} alt="house-4" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[4]} alt="house-5" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[5]} alt="house-6" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[6]} alt="house-7" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[7]} alt="house-8" />
-        </div>
-        <div className="border rounded">
-          <img src={props.image[8]} alt="house-9" />
-        </div>
+        {props.image.map((image, index) => (
+          <img
+            key={index}
+            src={image}
+            alt="house"
+            onClick={(event) => {
+              setSelectedImage(event.target.src)
+            }}
+            className="rounded-md w-full"
+          />
+        ))}
       </div>
     </div>
   )
