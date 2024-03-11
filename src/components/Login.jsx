be7e4c963391696538167ce5946bf060d6b7c7b7
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-03-11 11:17:33 +0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,32 @@
+import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
+
 function Login() {
+  const [error, setError] = useState('')
+
+  const navigate = useNavigate()
+
+  const submitForm = async (e) => {
+    e.preventDefault()
+    setError('')
+
+    const formData = new FormData(e.target)
+    const email = formData.get('email')
+    const password = formData.get('password')
+
+    const response = await axios.post('https://haiku-bnb.onrender.com/login', {
+      email,
+      password
+    })
+
+    if (response.data.error) {
+      setError(response.data.error)
+    } else {
+      navigate('/')
+    }
+  }
+
   return (
     <div className="grid grid-cols-3 gap-28">
       <div></div>
@@ -10,12 +38,16 @@ function Login() {
             width={80}
           />
         </div>
-        <form>
+        <form onSubmit={submitForm}>
           <div className="flex flex-col p-7">
             <div className="text-sm text-gray-400">Email</div>
-            <input type="email" className="border rounded h-10" />
+            <input type="email" name="email" className="border rounded h-10" />
             <div className="text-sm text-gray-400 mt-2">Password</div>
-            <input type="password" className="border rounded h-10" />
+            <input
+              type="password"
+              name="password"
+              className="border rounded h-10"
+            />
           </div>
           <div className="flex justify-center mt-1 p-2">
             <button className="flex justify-center border rounded bg-[#FB7185] text-white px-36 py-3">
@@ -23,6 +55,7 @@ function Login() {
             </button>
           </div>
         </form>
+        <div className="pl-8 text-red-600 text-sm">{error}</div>
         <div className="flex justify-start m-5 ml-6 text-sm">
           <p>
             New to Airbnb?
