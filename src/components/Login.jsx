a7995a42021cab614e386f7591c005bc87e1611f
ValeriaSml <114675105+ValeriaSml@users.noreply.github.com>
2024-03-11 12:32:53 +0700

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios'
-axios.defaults.withCredentials = true
 
 function Login() {
   const [error, setError] = useState('')
