1ef7091030000da8991393af4ed1d521e472170b
Yaroslav Guerra <Yaroslavguerra27@gmail.com>
2024-03-13 13:14:42 +0700

    update the bookings page to let the client see all the bookings that made

--- src/components/HouseCard.jsx
+++ src/components/HouseCard.jsx
@@ -10,7 +10,7 @@ function HouseCard(props) {
         <div>
           <div>
             <img
-              className=" rounded-t-md"
+              className=" h-52 w-full rounded-t-md"
               src={props.house.photo}
               alt="house"
             />
@@ -19,7 +19,7 @@ function HouseCard(props) {
             <h3 className="text-sm font-bold">{props.house.location}</h3>
             <div>
               <span className="text-xs text-gray-400 pr-2">
-                {props.house.bedrooms} bedrooms
+                {props.house.rooms} bedrooms
               </span>
               <span className="text-xs text-gray-400 pr-2">·</span>
               <span className="text-xs text-gray-400">
@@ -53,12 +53,12 @@ function HouseCard(props) {
           {props.isBooking && (
             <div className="grid bg-[#ECFDF5] rounded-2 p-2 m-2 place-content-center">
               <div className="flex">
-                <p className="text-xs ">{props.house.checkIn} -</p>
-                <p className="text-xs pl-1"> {props.house.checkOut}</p>
+                <p className="text-xs ">{props.house.from_date} -</p>
+                <p className="text-xs pl-1"> {props.house.to_date}</p>
               </div>
               <p className="flex text-xs font-bold gap-1 justify-center">
-                {props.house.totNights} nights = $
-                {props.house.totNights * props.house.price}
+                {props.house.nights} nights = $
+                {props.house.nights * props.house.price}
               </p>
             </div>
           )}
